09edf83f2cb3499b55e9843b8af8431e3d8e74d4
abi-seg <abirami1488@gmail.com>
2025-11-12 00:43:31 +0100

    Enregistrement du modèle Ticket dans le panneau d'administration

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ticket  # and other models like Review
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title','user','time_created')
+    search_fields = ('title', 'user__username')
+    list_filter = ('user',)
 
 # Register your models here.
