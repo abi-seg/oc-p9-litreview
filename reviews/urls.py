761fc179422c78f58bb4f965bb078cf584b0a08b
abi-seg <abirami1488@gmail.com>
2025-11-12 00:46:01 +0100

    Ajout des routes pour modifier et supprimer un ticket

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('flux/',views.feed_view, name='feed'),
     path('create_ticket/', views.create_ticket, name='create_ticket'), 
     path('ticket_succes/', views.ticket_succes, name='ticket_succes'),
-    path('edit_ticket/<int:ticket_id>/',views.edit_ticket, name='edit_ticket'),
+    path('ticket/<int:ticket_id>/edit/',views.edit_ticket, name='edit_ticket'),
     path('delete_ticket/<int:ticket_id>/',views.delete_ticket, name='delete_ticket'),
 ]
