eb56844e43dc2c92b5f87962297ac4fc8a997a46
abi-seg <abirami1488@gmail.com>
2025-11-12 23:57:21 +0100

    Enregistrement des routes pour create_review et select_ticket_to_review

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('ticket_succes/', views.ticket_succes, name='ticket_succes'),
     path('ticket/<int:ticket_id>/edit/',views.edit_ticket, name='edit_ticket'),
     path('delete_ticket/<int:ticket_id>/',views.delete_ticket, name='delete_ticket'),
+    path('ticket/<int:ticket_id>/review/', views.create_review, name='create_review'),
+    path('select_ticket_to_review/', views.select_ticket_to_review, name='select_ticket_to_review'),
+    path('create_review/', views.create_ticket_and_review, name='create_ticket_and_review'),
 ]
