7a740f8d73840ddc6883b9532470ad5f6681f7d0
abi-seg <abirami1488@gmail.com>
2025-11-12 23:50:42 +0100

    Personnalisation des widgets pour un affichage Bootstrap et champ note en ligne

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Ticket
+from .models import Ticket, Review
 
 class TicketForm(forms.ModelForm):
     class Meta:
@@ -13,3 +13,16 @@ class TicketForm(forms.ModelForm):
 
 class FollowUserForm(forms.Form):
     username = forms.CharField(label="Nom d'utilisateur", max_length=150)
+
+from django import forms
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['headline', 'rating', 'body']
+        widgets = {
+            'headline': forms.TextInput(attrs={'class': 'form-control'}),
+            'rating': forms.RadioSelect(),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
