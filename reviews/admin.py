bde29f0afe2d48e7875f6de57b5baaba04c56cc9
abi-seg <abirami1488@gmail.com>
2025-11-12 01:05:48 +0100

    Enregistrement du modèle UserFollows dans l'interface d'administartion

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ticket  # and other models like Review
+from .models import Ticket, UserFollows  # and other models like Review
 
 
 @admin.register(Ticket)
@@ -7,5 +7,10 @@ class TicketAdmin(admin.ModelAdmin):
     list_display = ('title','user','time_created')
     search_fields = ('title', 'user__username')
     list_filter = ('user',)
+
+@admin.register(UserFollows)
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user')
+    search_fields = ('user__username','followed_user__username')
 
 # Register your models here.
