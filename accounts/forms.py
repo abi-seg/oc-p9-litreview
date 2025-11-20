32dd33150390f298044483e39e1bd71b40a564a5
abi-seg <abirami1488@gmail.com>
2025-11-20 23:26:50 +0100

    Ajouter Docstrings

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,41 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import CustomUser
 
 class ConnexionForm(AuthenticationForm):
+    """
+    Formulaire de connexion personnalisé.
+
+    Cette classe étend le formulaire d'authentificaiton par défaut de Django
+    afin de personnaliser l'affichage des champs <<Nom d'utilisateur>> et <<Mot de passe>>.
+    Elle applique également des classes CSS spécifiques aux widgets pour
+    facilitier l'intégration dans l'interface utilisateur.
+
+    """
+
     username = forms.CharField(label="Nom d'utilisateur", widget=forms.TextInput
                                (attrs={'class': 'form-control'}))
     password = forms.CharField(label="Mot de passe",widget=forms.PasswordInput(
                  attrs={'class': 'form-control'}))
     
 class InscriptionForm(UserCreationForm):
+    """
+
+    Formulaire d'inscription personnalisé.
+
+    Cette classe étend le formulaire de création d'utilisateur de Django afin d'utilisateur
+    le modèle 'CustomUser' défini dans l'application. Elle permet de personnaliser les labels des champs
+    ainsi que les classes CSS des widgets pour un rendu cohérent avec le style de l'application.
+
+    """
     class Meta:
         model = CustomUser
         fields = ['username']
+
+        """
+        Initialise le formulaire en modifiant les labels et les attributs des widgets
+        pour les champs <<Nom d'utilisateur>>, <<Mot de passe>> et
+        <<Confirmer mot de passe>>.
+
+        """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,15 @@ from .forms import ConnexionForm, InscriptionForm
 from django.contrib.auth.forms import AuthenticationForm
 
 def connexion_view(request):
+    """
+    Gère la connexion d'un utilisateur.
+    si la méthode est POST, vérifie les informations de connnexion avec la formulaire personnalisé.
+    si elles sont valides, connecte l'utilisateur et le redirige vers la page du flux.
+    sinon, affiche le formulaire de connexion.
+    : param request: HttpRequest de Django
+    : return: Page de connexion avec formulaire ou redirection
+
+    """
     if request.method == 'POST':
         form = ConnexionForm(request, data = request.POST)
         if form.is_valid():
@@ -15,6 +24,16 @@ def connexion_view(request):
     return render(request, 'accounts/connexion.html', {'form': form})
 
 def inscription_view(request):
+     """
+     Gère l'inscription d'un nouvel utilisateur.
+
+     si la méthode est POST et que le formulaire est valide,
+     l'utilisateur est enregistré, connecté automatiquement, puis redirigé vers le flux.
+     sinon, affiche le formulaire d'inscription vide ou avec erreurs.
+     :param request: HttpRequest de Django
+     :return: Page d'inscription avec formulaire ou redirection
+     
+     """
      if request.method == 'POST':
         form = InscriptionForm(request.POST)
         if form.is_valid():
